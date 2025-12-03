b9a2b8d3a038d20c1235d1dca9407bd47ab369f3
nosach-oleksii <nosach.oleksii@gmail.com>
2025-12-03 22:03:36 +0100

    try use fixture

--- tests/04-Delete_user_User_state.spec.ts
+++ tests/04-Delete_user_User_state.spec.ts
@@ -1,25 +1,13 @@
-import { test, expect, Locator } from "@playwright/test";
-import { userList } from "../test-data/users";
-import { HomePage } from "../pom/pages/HomePage";
-import { SettingsPage } from "../pom/pages/SettingsPage";
-import { Sidebar } from "../pom/pages/Sidebar";
-
-let homePage: HomePage;
-let sidebar: Sidebar;
-let settingsPage: SettingsPage;
+import { pagesFixture } from "../fixtures/pages.ts";
+import { test, expect } from "@playwright/test";
 
 test.describe("Login and Delete user", () => {
-    test.beforeEach(async ({ page }) => {
-        homePage = new HomePage(page);
-        sidebar = new Sidebar(page);
-        settingsPage = new SettingsPage(page);
-    });
-    test("Login User", async ({ page }) => {
-        await homePage.open("/");
-        await homePage.openLogInForm();
-        await homePage.enterEmailLogin(userList.mainUser.email);
-        await homePage.enterPasswordLogin(userList.mainUser.password);
-        await homePage.clickLogInButton();
+    test("Login User", async ({ page, pages }) => {
+        await pages.homePage.open("/");
+        await pages.homePage.openLogInForm();
+        await pages.homePage.enterEmailLogin(userList.mainUser.email);
+        await pages.homePage.enterPasswordLogin(userList.mainUser.password);
+        await pages.homePage.clickLogInButton();
         await page.waitForFunction(() => document.cookie.includes("sid="));
         await page.context().storageState({ path: "test-data/states/storageState.json" });
     });
@@ -27,15 +15,12 @@ test.describe("Login and Delete user", () => {
 
 test.describe("Delete User", () => {
     test.use({ storageState: "test-data/states/storageState.json" });
-    test("Delete User", async ({ page }) => {
-        homePage = new HomePage(page);
-        sidebar = new Sidebar(page);
-        settingsPage = new SettingsPage(page);
-        await homePage.open("/");
-        await sidebar.verifySidebarVisible();
-        await sidebar.clickSettingsButton();
-        await settingsPage.isVisible();
-        await settingsPage.removeAccount();
-        await settingsPage.clickRemove();
+    test("Delete User", async ({ page, pages: Pages }) => {
+        await pages.homePage.open("/");
+        await pages.sidebar.verifySidebarVisible();
+        await pages.sidebar.clickSettingsButton();
+        await pages.settingsPage.isVisible();
+        await pages.settingsPage.removeAccount();
+        await pages.settingsPage.clickRemove();
     });
 });
